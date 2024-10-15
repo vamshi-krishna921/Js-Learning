aec86fd7a8c3879ab1457bf1bf866d3901b00b8c
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2024-10-15 19:49:07 +0530

    Q-months

--- Search_filter_Array.js
+++ Search_filter_Array.js
@@ -20,3 +20,18 @@ const num= [1,2,3,6,4,5,6,7,8,9,""];
 console.log(num.includes(3));
 console.log(num.includes(3,2));
 console.log(num.includes(3,5));
+
+//! Solve it
+//Add "Dec" at end,Update march to "March",Delete june from array
+let months = ["Jan","march","April","June","July"];
+//Add "Dec" at end
+months.push("Dec");
+console.log(months);
+// Update march to "March"
+let Update = months.indexOf("march");
+months.splice(Update,1,"March")
+console.log(months);
+// Delete june from array
+let Delete = months.indexOf("June");
+months.splice(Delete,1);
+console.log(months);
