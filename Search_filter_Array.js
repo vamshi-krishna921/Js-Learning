2f3f9b5dde4a11d39b2ba335f4ce602629c1a025
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2024-10-15 19:13:50 +0530

    Includes

--- Search_filter_Array.js
+++ Search_filter_Array.js
@@ -1,4 +1,4 @@
-//TODO Indexof()
+//TODO IndexOf()
 
 const nums= [1,2,3,4,5,6,7,8,9,0];
 console.log(nums.indexOf(8)); // 7
@@ -13,3 +13,10 @@ console.log(numbers.lastIndexOf(6,5));
 console.log(numbers.lastIndexOf(10));
 console.log(numbers);
 console.log(numbers.lastIndexOf(""));
+
+//TODO Includes()
+
+const num= [1,2,3,6,4,5,6,7,8,9,""];
+console.log(num.includes(3));
+console.log(num.includes(3,2));
+console.log(num.includes(3,5));
