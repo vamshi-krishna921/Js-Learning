6e05337482080bf103561fab0413bd49cafb04e7
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2024-10-20 13:35:15 +0530

    Sorting

--- sortArray.js
+++ sortArray.js
@@ -1,9 +1,32 @@
 //Sorting and comparing array 
-
+//! Mostly it does not work on numbers until we use sort function which is in line 11.
 const numbers = [1,2,3,6,4,5,6,7,8,9];
 numbers.sort();
-console.log(numbers);
+console.log(numbers);//[1,2,3,4,5,6,6,7,8,9]
 
 const nums = [10,22,3,88,0,100,33];
 nums.sort();
-console.log(nums);
+console.log(nums); // [0, 10, 100, 22,3, 33,  88]
+
+//? Ascending and decending order
+
+//Ascending Order
+nums.sort((a,b)=> 
+    {
+      if (a>b) 
+      return 1;
+      if (a<b)
+      return -1;
+    });
+    console.log(nums);//[0,  3,  10, 22,33, 88, 100]
+
+//Decending Order
+
+nums.sort((a,b)=> 
+    {
+      if (a>b) 
+      return -1;
+      if (a<b)
+      return 1;
+    });
+    console.log(nums);//[100, 88, 33, 22, 10,  3,  0]
