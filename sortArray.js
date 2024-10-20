21656bea26967ead11af3a260b83ba28e93e5679
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2024-10-20 13:34:22 +0530

    Sorting

---
+++ sortArray.js
@@ -0,0 +1,9 @@
+//Sorting and comparing array 
+
+const numbers = [1,2,3,6,4,5,6,7,8,9];
+numbers.sort();
+console.log(numbers);
+
+const nums = [10,22,3,88,0,100,33];
+nums.sort();
+console.log(nums);
