1349e8af1d8fde440e1461bc1f8844702d870a54
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2024-10-22 22:37:19 +0530

    escape character

--- Strings.js
+++ Strings.js
@@ -2,3 +2,9 @@
 //TODO String length
 const str = "Hello, World!"
 console.log(str.length());// 13
+
+//TODO Escape character
+
+const solarSystem = "The solar system has \'6\' planets . \n We are living in \"Earth\".";
+console.log(solarSystem);//The solar system has '6' planets . 
+                         //We are living in "Earth".
