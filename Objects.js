23c6ef4c9a674f568b9747e68769e6c004e5442e
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-01-29 05:26:48 +0530

    Objects 1

---
+++ Objects.js
@@ -0,0 +1,7 @@
+const persons = {name : "Jai", place : "Our place", class : "Learning"};
+console.log(persons); // {name: 'Jai', place: 'Our place', class: 'Learning'}
+console.log(persons.name); // Jai
+console.log(persons.place); // Our place
+console.log(persons["class"]); // Learning
+
+
