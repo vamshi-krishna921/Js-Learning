b643847efdac4fa41c11167dd11ecd2e537068f4
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-01-29 05:34:33 +0530

    Objects add and modify

--- Objects.js
+++ Objects.js
@@ -1,7 +1,12 @@
-const persons = {name : "Jai", place : "Our place", class : "Learning"};
+let persons = {name : "Jai", place : "Our place", class : "Learning"};
 console.log(persons); // {name: 'Jai', place: 'Our place', class: 'Learning'}
 console.log(persons.name); // Jai
 console.log(persons.place); // Our place
 console.log(persons["class"]); // Learning
 
+//* Adding / Modifying properties and values in object.
+
+persons.section = "A"; 
+persons.class = "student";
+console.log(persons); // {name: 'Jai', place: 'Our place', class: 'student', section: 'A'}
 
