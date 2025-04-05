d4dd115dfbe5a82364b0b3eea83759cbb8a0b294
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-04-05 06:27:47 +0530

    05/04/25

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -2,28 +2,28 @@
 
 let cars = ["BMW", "Volvo", "Benz", "Rolls Royce", "Breeza"];
 cars.push("Lamborghini");
-console.log(cars.push());// New length of array after adding
+console.log(cars.push()); // New length of array after adding
 console.log(cars);
 
 //TODO Pop()
 
 let cars = ["BMW", "Volvo", "Benz", "Rolls Royce", "Breeza"];
 cars.pop(2);
-console.log(cars.pop(2));// Gives the deleted element
+console.log(cars.pop(2)); // Gives the deleted element
 console.log(cars);
 
-//TODO unshift 
+//TODO unshift
 
 let cars = ["BMW", "Volvo", "Benz", "Rolls Royce", "Breeza"];
 cars.unshift("Lamborghini");
-console.log(cars.unshift());// New length of array after adding
+console.log(cars.unshift()); // New length of array after adding
 console.log(cars);
 
-//TODO shift 
+//TODO shift
 
 let cars = ["BMW", "Volvo", "Benz", "Rolls Royce", "Breeza"];
 cars.shift();
-console.log(cars.shift());// Gives the deleted element
+console.log(cars.shift()); // Gives the deleted element
 console.log(cars);
 
 //TODO splice()
@@ -31,14 +31,45 @@ let cars = ["BMW", "Volvo", "Benz", "Rolls Royce", "Breeza"];
 let delElem = prompt("Enter the deleted index number : ");
 delElem = parseInt(delElem);
 // cars.splice(1,"Atlo");
-cars.splice(delElem,1);
+cars.splice(delElem, 1);
 console.log(cars);
 // cars.splice(2);
 
 //TODO Slice()
 
 let cars = ["BMW", "Volvo", "Benz", "Rolls Royce", "Breeza"];
-cars.slice(1,3);
+cars.slice(1, 3);
 console.log(cars.slice());
 console.log(cars);
 
+// Todo every()
+
+let array = [1, 3, 3, 5];
+let sol = array.every(sum);
+console.log(sol); // true
+function sum(el) {
+  if (el % 2 != 0) return true;
+  else return false;
+}
+console.log(
+  array.every(function (el) {
+    el % 2 === 0;
+  })
+); //false
+console.log(array.every((el) => el % 2 != 0)); // true
+
+// Todo max using reduce
+let array1 = [2, 44, 55, 46, 88, 99];
+let max = array1.reduce((max, el) => {
+  if (el > max) return el;
+  else return max;
+});
+console.log(max); // 99
+
+// Todo min using reduce
+let array2 = [2, 44, 55, 46, 88, 99];
+let min = array2.reduce((min, el) => {
+  if (el < min) return el;
+  else return min;
+});
+console.log(min); // 2
--- this.js
+++ this.js
@@ -4,7 +4,12 @@ let student = {
     rank : this, //(Window {0: global, window: Window, self: Window, document: document, name: '', location: Location, …}[[Prototype]]: Object)
     get : function () {
         console.log(this);
+    },
+    get1 : function (){
+        console.log(this.Name); // Krishna
+     
     }
 }
 student.get; // ƒ () {console.log(this);}
 student.get(); // {Name: 'Krishna', age: 22, rank: Window, get: ƒ}
+student.get1(); 
