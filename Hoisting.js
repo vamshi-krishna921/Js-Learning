fe7acea987df0c6000aa4674e5f606b4c1c414d9
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-05-21 19:01:40 +0530

    Hoisting

---
+++ Hoisting.js
@@ -0,0 +1,8 @@
+console.log(num); // undefined
+var num = 1;
+console.log(num); // 1
+
+
+console.log(num1); // ReferenceError: Cannot access 'num1' before initialization
+let num1 = 2;
+console.log(num1); // 2
