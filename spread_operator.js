6376d16eb792676ab8ad031d9f88ca73ec5efc29
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-05-24 19:56:35 +0530

    spread-operator

--- spread_operator.js
+++ spread_operator.js
@@ -1,9 +1,19 @@
-let a = [1,2,3,4];
+//* Copying an Array
+let a = [1, 2, 3, 4, 5];
 let b = [...a];
 let c = a;
-console.log(a); // [1, 2, 3, 4]
-console.log(b); // [1, 2, 3, 4]
+console.log(a); // [1, 2, 3, 4 , 5]
+console.log(b); // [1, 2, 3, 4 , 5]
 a.push(5);
-console.log(a); // [1, 2, 3, 4, 5]
+console.log(a); // [1, 2, 3, 4, 5 , 5]
 console.log(b); // [1, 2, 3, 4]
-console.log(c); // [1, 2, 3, 4, 5]
+console.log(c); // [1, 2, 3, 4, 5 , 5]
+
+//* Concatenating/Combining Arrays
+let d = [5, 6, 7];
+let e = [...a, ...d];
+console.log(e); // [1, 2, 3, 4, 5, 5, 5, 6, 7]
+
+//* Adding Elements to Existing Array
+b.push(...[5, 6, 7, 8, 9, 10]);
+console.log(b); // [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10]
