8c8e72b1a43ae8be43bd75c3c1f44bf5427e2f13
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-06-28 08:26:28 +0530

    async/await

--- fetch.js
+++ fetch.js
@@ -12,3 +12,41 @@ fetch(url)
   .catch((err) => {
     console.log("error", err);
   });
+
+//* async returns a promise always
+
+async function greet() {
+  return "Namaste";
+}
+greet().then((res) => console.log(res)); //Namaste
+
+let greet1 = async () => {
+  return "Hello";
+};
+greet1().then((res) => console.log(res)); //Hello
+
+//* await is used inside async used to pause the execution os async fn until promise is settled.
+
+function rand() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let random = Math.random() + 10;
+      console.log(random.toFixed());
+      reject();
+    }, 1000);
+  });
+}
+async function randCall() {
+  await rand();
+  await rand();
+  await rand();
+  await rand();
+  rand();
+}
+randCall();
+// output : after 1 sec promsie is resolved and output comes one after another 
+// 10
+// 11
+// 10
+// 11
+// 11
